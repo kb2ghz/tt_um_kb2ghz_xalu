6f3a0bbe56e973acca39d2601a0b9ed84346e3df
Mike McCann <kb2ghz@gmail.com>
2024-07-21 15:41:09 -0400

    Update tt_um_kb2ghz_xalu.v

--- src/tt_um_kb2ghz_xalu.v
+++ src/tt_um_kb2ghz_xalu.v
@@ -91,7 +91,7 @@ wire bit0cy, bit1cy, bit2cy;  // carry signals between full adders
 wire ADD, AND, OR, XOR, PASSA, PASSB, SHL, SHR;
 wire d0int, d1int, d2int, d3int;
 
-	assign d0int = (ADD & ('da0 ^ 'db0 ^ 'ci_right)) |
+assign d0int = (ADD & ('da0 ^ 'db0 ^ 'ci_right)) |
 		(AND & 'da0 & 'db0)   |
 		(OR & ('da0 | 'db0))  |
 		(XOR & ('da0 ^ 'db0)) |
@@ -100,7 +100,7 @@ wire d0int, d1int, d2int, d3int;
 		(SHL & 'ci_right) |
 		(SHR & 'da1);
 
-	assign d1int = (ADD & ('da1 ^ 'db1 ^ bit0cy)) |
+assign d1int = (ADD & ('da1 ^ 'db1 ^ bit0cy)) |
 		(AND & 'da1 & 'db1)   |
 		(OR & ('da1 | 'db1))  |
 		(XOR & ('da1 ^ 'db1)) |
@@ -109,7 +109,7 @@ wire d0int, d1int, d2int, d3int;
 		(SHL & 'da0) |
 		(SHR & 'da2);
 
-	assign d2int = (ADD & ('da2 ^ 'db2 ^ bit1cy)) |
+assign d2int = (ADD & ('da2 ^ 'db2 ^ bit1cy)) |
 		(AND & 'da2 & 'db2)   |
 		(OR & ('da2 | 'db2))  |
 		(XOR & ('da2 ^ 'db2)) |
@@ -118,7 +118,7 @@ wire d0int, d1int, d2int, d3int;
 		(SHL & 'da1) |
 		(SHR & 'da3);
 
-	assign d3int = (ADD & ('da3 ^ 'db3 ^ bit2cy)) |
+assign d3int = (ADD & ('da3 ^ 'db3 ^ bit2cy)) |
 		(AND & 'da3 & 'db3)   |
 		(OR & ('da3 | 'db3))  |
 		(XOR & ('da3 ^ 'db3)) |
@@ -127,9 +127,9 @@ wire d0int, d1int, d2int, d3int;
 		(SHL & 'da2) |
 		(SHR & 'ci_left);
 
-	assign bit0cy = 'da0 & 'db0 | 'ci_right & ('da0 | 'db0);
-	assign bit1cy = 'da1 & 'db1 | bit0cy & ('da1 | 'db1);
-	assign bit2cy = 'da2 & 'db2 | bit1cy & ('da2 | 'db2);
+assign bit0cy = 'da0 & 'db0 | 'ci_right & ('da0 | 'db0);
+assign bit1cy = 'da1 & 'db1 | bit0cy & ('da1 | 'db1);
+assign bit2cy = 'da2 & 'db2 | bit1cy & ('da2 | 'db2);
 
 // inverting output mode
 
@@ -151,7 +151,7 @@ assign SHL = 'F2 & 'F1 & 'F0;        // 7
 
 // carry outputs
 
-	assign 'co_left = (SHL & 'da3) | (ADD & ('da3 & 'db3 | bit2cy & ('da3 | 'db3)));
+assign 'co_left = (SHL & 'da3) | (ADD & ('da3 & 'db3 | bit2cy & ('da3 | 'db3)));
 assign 'co_right = SHR & 'da0;
 
 // output status
@@ -159,7 +159,7 @@ assign 'co_right = SHR & 'da0;
 assign ZERO = ~'d0 & ~'d1 & ~'d2 & ~'d3;
 assign NEG_ZERO = 'd0 & 'd1 & 'd2 & 'd3;
 
-	assign 'EQU = (('da0 & 'db0) | (~'da0 & ~'db0)) &
+assign 'EQU = (('da0 & 'db0) | (~'da0 & ~'db0)) &
 		(('da1 & 'db1) | (~'da1 & ~'db1)) &
 		(('da2 & 'db2) | (~'da2 & ~'db2)) &
 		(('da3 & 'db3) | (~'da3 & ~'db3));
