68aa5ba3452e314aa653ed9963437f53214b2a99
Mike McCann <kb2ghz@gmail.com>
2024-07-25 16:13:08 -0400

    Update tt_um_kb2ghz_xalu.v

--- src/tt_um_kb2ghz_xalu.v
+++ src/tt_um_kb2ghz_xalu.v
@@ -62,10 +62,10 @@ assign uio_oe = 8'b00001001;
 
 // complment output mode
 
-`define COM uio_out[3]  
+`define COM uio_in[3]  
 
 // list unused inputs to prevent warnings
-	wire _unused =&{ena,clk, uio_in[0], uio_in[3], uio_in[7], uio_out[1], uio_out[2], uio_out[4], uio_out[5], uio_out[6], uio_out[7], rst_n, 1'b0};
+wire _unused =&{ena,clk, uio_in[0], uio_in[7], uio_out[1], uio_out[2], uio_out[4], uio_out[5], uio_out[6], uio_out[7], rst_n, 1'b0};
 
 wire bit0cy, bit1cy, bit2cy;  // carry signals between full adders
 
