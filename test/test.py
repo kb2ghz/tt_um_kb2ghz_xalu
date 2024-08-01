e9f05477a53c43decc15a8cf93d5322ef560d0a1
Mike McCann <kb2ghz@gmail.com>
2024-08-01 11:51:36 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -59,11 +59,11 @@ async def test_project(dut):
     assert dut.uio_out.value == int("00000000",2)
 
     # AND function test D
-    #dut.ui_in.value = int("10101010",2)
-    #dut.uio_in.value = int("00010000",2)
-    #await ClockCycles(dut.clk, 10)
-    #assert dut.uo_out.value == int("01001010",2)
-    #assert dut.uio_out.value == int("00000000",2)
+    dut.ui_in.value = int("10101010",2)
+    dut.uio_in.value = int("00010000",2)
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == int("01001010",2)
+    assert dut.uio_out.value == int("00000000",2)
 
     # OR test
     # XOR test
