a85a3a11115c0adad8ec8acf0c53520a35222edd
Mike McCann <kb2ghz@gmail.com>
2024-08-03 13:29:07 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -65,7 +65,21 @@ async def test_project(dut):
     assert dut.uo_out.value == int("01001010",2)
     assert dut.uio_out.value == int("00000000",2)
 
-    # OR test
+    # OR function test A
+    dut.ui_in.value = int("00001111",2)
+    dut.uio_in.value = int("00100000",2)
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == int("00001111",2)
+    assert dut.uio_out.value == int("00000001",2)
+
+    # OR function test B
+    dut.ui_in.value = int("11110000",2)
+    dut.uio_in.value = int("00100000",2)
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == int("00001111",2)
+    assert dut.uio_out.value == int("00000001",2)
+
+
     # XOR test
     # PASSA and PASSB tests
     # SHR test
