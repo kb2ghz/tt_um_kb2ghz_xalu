05adf7a3ebf9a122b9250652f4b1461dad21d8db
Mike McCann <kb2ghz@gmail.com>
2024-08-06 07:41:12 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -178,9 +178,24 @@ async def test_project(dut):
     assert dut.uio_out.value == int("00000000",2)
 
     # SHL function test A
+    dut.ui_in.value = int("00001111",2)
+    dut.uio_in.value = int("01110000",2)
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == int("00011110",2)
+    assert dut.uio_out.value == int("00000000",2)
+
     # SHL function test B
+    dut.ui_in.value = int("00001111",2)
+    dut.uio_in.value = int("01110100",2)
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == int("00011111",2)
+    assert dut.uio_out.value == int("00000001",2)
+
     # SHL function test C
+
+
     # SHL function test D
+
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
